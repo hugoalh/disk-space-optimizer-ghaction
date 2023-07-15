e493dfe1d6f6edbb77a7d3b4e7af4d1be9bc6c14
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-07-15 18:13:56 +0800

    20230715A

--- main.ps1
+++ main.ps1
@@ -9,122 +9,90 @@ Write-Host -Object 'Initialize.'
 [Boolean]$OsIsMac = $Env:RUNNER_OS -ieq 'MacOS'
 [Boolean]$OsIsWindows = $Env:RUNNER_OS -ieq 'Windows'
 If ($True -inotin @($OsIsLinux, $OsIsMac, $OsIsWindows)) {
-	Write-GitHubActionsError -Message "``$Env:RUNNER_OS`` is not an supported runner OS!"
+	Write-GitHubActionsWarning -Message "``$Env:RUNNER_OS`` is not an supported runner OS!"
 	Exit 0
 }
-[String]$ListPathOsPropertyName = "Path$($Env:RUNNER_OS)"
+[String]$OsPathPropertyName = "Path$($Env:RUNNER_OS)"
 [Boolean]$APTProgramIsExist = $Null -ine (Get-Command -Name 'apt-get' -CommandType 'Application' -ErrorAction 'SilentlyContinue')
 [ScriptBlock]$APTCommandListPackage = {
-	apt list --installed *>&1
+	apt list --installed *>&1 |
+		Write-Output
 }
 [ScriptBlock]$APTCommandUninstallPackage = {
 	Param ([String[]]$InputObject = @())
 	ForEach ($_ In $InputObject) {
-		apt-get --assume-yes remove $_ *>&1
+		apt-get --assume-yes remove $_ *>&1 |
+			Write-Output
 	}
 }
 [Boolean]$ChocolateyProgramIsExist = $Null -ine (Get-Command -Name 'choco' -CommandType 'Application' -ErrorAction 'SilentlyContinue')
 [ScriptBlock]$ChocolateyCommandListPackage = {
-	choco list --include-programs --no-progress --prerelease *>&1
+	choco list --include-programs --no-progress --prerelease *>&1 |
+		Write-Output
 }
 [ScriptBlock]$ChocolateyCommandUninstallPackage = {
 	Param ([String[]]$InputObject = @())
 	ForEach ($_ In $InputObject) {
-		choco uninstall $_ --ignore-detected-reboot --yes *>&1
+		choco uninstall $_ --ignore-detected-reboot --yes *>&1 |
+			Write-Output
 	}
 }
 [Boolean]$DockerProgramIsExist = $Null -ine (Get-Command -Name 'docker' -CommandType 'Application' -ErrorAction 'SilentlyContinue')
 [ScriptBlock]$DockerCommandListImage = {
-	docker image ls --all --format '{{json .}}' *>&1
+	docker image ls --all --format '{{json .}}' *>&1 |
+		Write-Output
 }
 [ScriptBlock]$DockerCommandPruneImage = {
-	docker image prune --force *>&1
+	docker image prune --force *>&1 |
+		Write-Output
 }
 [ScriptBlock]$DockerCommandRemoveImage = {
 	Param ([String[]]$InputObject = @())
 	ForEach ($_ In $InputObject) {
-		docker image rm $_ *>&1
+		docker image rm $_ *>&1 |
+			Write-Output
 	}
 }
 [Boolean]$HomebrewProgramIsExist = $Null -ine (Get-Command -Name 'brew' -CommandType 'Application' -ErrorAction 'SilentlyContinue')
 [ScriptBlock]$HomebrewCommandListPackage = {
-	brew list -1 --versions *>&1
+	brew list -1 --versions *>&1 |
+		Write-Output
 }
 [ScriptBlock]$HomebrewCommandUninstallPackage = {
 	Param ([String[]]$InputObject = @())
 	ForEach ($_ In $InputObject) {
-		brew uninstall $_ *>&1
+		brew uninstall $_ *>&1 |
+			Write-Output
 	}
 }
 [Boolean]$NPMProgramIsExist = $Null -ine (Get-Command -Name 'npm' -CommandType 'Application' -ErrorAction 'SilentlyContinue')
 [ScriptBlock]$NPMCommandListPackage = {
-	npm --global list *>&1
+	npm --global list *>&1 |
+		Write-Output
 }
 [ScriptBlock]$NPMCommandUninstallPackage = {
 	Param ([String[]]$InputObject = @())
 	ForEach ($_ In $InputObject) {
-		npm --global uninstall $_ *>&1
+		npm --global uninstall $_ *>&1 |
+			Write-Output
 	}
 }
 [Boolean]$PipxProgramIsExist = $Null -ine (Get-Command -Name 'pipx' -CommandType 'Application' -ErrorAction 'SilentlyContinue')
 [ScriptBlock]$PipxCommandListPackage = {
-	pipx list --json *>&1
+	pipx list --json *>&1 |
+		Write-Output
 }
 [ScriptBlock]$PipxCommandUninstallPackage = {
 	Param ([String[]]$InputObject = @())
 	ForEach ($_ In $InputObject) {
-		pipx uninstall $_ *>&1
+		pipx uninstall $_ *>&1 |
+			Write-Output
 	}
 }
 [String]$SessionId = (
 	New-Guid |
 		Select-Object -ExpandProperty 'Guid'
 ).ToUpper() -ireplace '-', ''
-Function Get-ProgramTree {
-	[CmdletBinding()]
-	[OutputType([Void])]
-	Param ()
-	[String[]]$ProgramTreePath = @()
-	If ($OsIsLinux) {
-		$ProgramTreePath += @(
-			$Env:AGENT_TOOLSDIRECTORY,
-			$Env:HOME,
-			'/opt',
-			'/usr/bin',
-			'/usr/lib',
-			'/usr/local',
-			'/usr/sbin',
-			'/usr/share'
-		)
-	}
-	If ($OsIsMac) {
-		$ProgramTreePath += @(
-			$Env:AGENT_TOOLSDIRECTORY,
-			$Env:HOME,
-			'/Applications',
-			'/opt',
-			'/Users/runner',
-			'/usr/bin',
-			'/usr/lib',
-			'/usr/local',
-			'/usr/sbin',
-			'/usr/share'
-		)
-	}
-	If ($OsIsWindows) {
-		$ProgramTreePath += @(
-			'C:\',
-			'D:\'
-		)
-	}
-	$ProgramTreePath |
-		ForEach-Object -Process {
-			Get-ChildItem -LiteralPath $_ -Recurse -Depth 2 -Force -ErrorAction 'Continue'
-		} |
-		Select-Object -ExpandProperty 'FullName' |
-		Sort-Object -Unique |
-		Write-Host
-}
 Function Get-DiskSpace {
 	[CmdletBinding()]
 	[OutputType([String])]
@@ -138,6 +106,97 @@ Function Get-DiskSpace {
 		df -h |
 			Join-String -Separator "`n" |
 			Write-Output
+	}
+}
+Function Show-TreeDetail {
+	[CmdletBinding()]
+	[OutputType([Void])]
+	Param (
+		[Parameter(Mandatory = $True, Position = 0)][String]$Stage
+	)
+	Enter-GitHubActionsLogGroup -Title "Path ($Stage): "
+	[String[]]$PathTree = @()
+	If ($OsIsLinux) {
+		$PathTree += @(
+			$Env:AGENT_TOOLSDIRECTORY,
+			$Env:HOME,
+			'/opt',
+			'/usr/bin',
+			'/usr/lib',
+			'/usr/local',
+			'/usr/sbin',
+			'/usr/share'
+		)
+	}
+	If ($OsIsMac) {
+		$PathTree += @(
+			$Env:AGENT_TOOLSDIRECTORY,
+			$Env:HOME,
+			'/Applications',
+			'/opt',
+			'/Users/runner',
+			'/usr/bin',
+			'/usr/lib',
+			'/usr/local',
+			'/usr/sbin',
+			'/usr/share'
+		)
+	}
+	If ($OsIsWindows) {
+		$PathTree += @(
+			$Env:APPDATA,
+			$Env:LOCALAPPDATA,
+			'C:\',
+			'C:\Program Files (x86)',
+			'C:\Program Files',
+			'C:\ProgramData',
+			'C:\Users',
+			'D:\'
+		)
+	}
+	$PathTree |
+		ForEach-Object -Process {
+			Get-ChildItem -LiteralPath $_ -Recurse -Depth 2 -Force -ErrorAction 'Continue'
+		} |
+		Select-Object -ExpandProperty 'FullName' |
+		Sort-Object -Unique |
+		Write-Host
+	Exit-GitHubActionsLogGroup
+	If ($APTProgramIsExist) {
+		Enter-GitHubActionsLogGroup -Title "APT ($Stage): "
+		Invoke-Command -ScriptBlock $APTCommandListPackage |
+			Write-Host
+		Exit-GitHubActionsLogGroup
+	}
+	If ($ChocolateyProgramIsExist) {
+		Enter-GitHubActionsLogGroup -Title "Chocolatey ($Stage): "
+		Invoke-Command -ScriptBlock $ChocolateyCommandListPackage |
+			Write-Host
+		Exit-GitHubActionsLogGroup
+	}
+	If ($DockerProgramIsExist) {
+		Enter-GitHubActionsLogGroup -Title "Docker ($Stage): "
+		Invoke-Command -ScriptBlock $DockerCommandListImage |
+			Write-Host
+		Exit-GitHubActionsLogGroup
+	}
+	If ($HomebrewProgramIsExist) {
+		Enter-GitHubActionsLogGroup -Title "Homebrew ($Stage): "
+		Invoke-Command -ScriptBlock $HomebrewCommandListPackage |
+			Write-Host
+		Exit-GitHubActionsLogGroup
+	}
+	If ($NPMProgramIsExist) {
+		Enter-GitHubActionsLogGroup -Title "NPM ($Stage): "
+		Invoke-Command -ScriptBlock $NPMCommandListPackage |
+			Write-Host
+		Exit-GitHubActionsLogGroup
+	}
+	If ($PipxProgramIsExist) {
+		Enter-GitHubActionsLogGroup -Title "Pipx ($Stage): "
+		Invoke-Command -ScriptBlock $PipxCommandListPackage |
+			Write-Host
+		Exit-GitHubActionsLogGroup
 	}
 }
 Function Test-StringMatchRegEx {
@@ -170,9 +229,7 @@ Function Test-StringMatchRegEx {
 	Out-String -Width 120 |
 	Write-GitHubActionsDebug
 If (Get-GitHubActionsDebugStatus) {
-	Enter-GitHubActionsLogGroup -Title 'Program Tree (Before): '
-	Get-ProgramTree
-	Exit-GitHubActionsLogGroup
+	Show-TreeDetail -Stage 'Before'
 }
 Write-Host -Object 'Import input.'
 [RegEx]$InputListDelimiter = Get-GitHubActionsInput -Name 'input_listdelimiter' -Mandatory -EmptyStringAsNull
@@ -199,24 +256,15 @@ Write-Host -Object 'Import input.'
 [Boolean]$RemoveNpmCache = [Boolean]::Parse((Get-GitHubActionsInput -Name 'cache_npm' -Mandatory -EmptyStringAsNull))
 [Boolean]$RemoveLinuxSwap = [Boolean]::Parse((Get-GitHubActionsInput -Name 'swap' -Mandatory -EmptyStringAsNull))
 Write-Host -Object 'Resolve operation.'
-[String[]]$DockerImageList = @()
+[String[]]$DockerImageListRaw = @()
 [String[]]$DockerImageRemoveQueue = @()
 [PSCustomObject[]]$GeneralRemoveQueue = @()
 If ($DockerProgramIsExist -and $RemoveDockerImageInclude.Count -gt 0) {
 	If ($OperationAsync) {
-		$Null = Start-Job -Name "$SessionId/Docker/List" -ScriptBlock {
-			docker image ls --all --format '{{json .}}' |
-				Join-String -Separator ',' -OutputPrefix '[' -OutputSuffix ']' |
-				ConvertFrom-Json -Depth 100 |
-				ForEach-Object -Process { "$($_.Repository)$(($_.Tag.Length -gt 0) ? ":$($_.Tag)" : '')" } |
-				Write-Output
-		}
+		$Null = Start-Job -Name "$SessionId/Docker/List" -ScriptBlock $DockerCommandListImage
 	}
 	Else {
-		$DockerImageList += docker image ls --all --format '{{json .}}' |
-			Join-String -Separator ',' -OutputPrefix '[' -OutputSuffix ']' |
-			ConvertFrom-Json -Depth 100 |
-			ForEach-Object -Process { "$($_.Repository)$(($_.Tag.Length -gt 0) ? ":$($_.Tag)" : '')" }
+		$DockerImageListRaw += Invoke-Command -ScriptBlock $DockerCommandListImage
 	}
 }
 If ($RemoveGeneralInclude.Count -gt 0) {
@@ -230,15 +278,18 @@ If ($RemoveGeneralInclude.Count -gt 0) {
 			($NPMProgramIsExist -and $_.NPM.Count -gt 0) -or
 			($PipxProgramIsExist -and $_.Pipx.Count -gt 0) -or
 			$_.Env.Count -gt 0 -or
-			$_.($ListPathOsPropertyName).Count -gt 0
+			$_.($OsPathPropertyName).Count -gt 0
 		} |
 		Where-Object -FilterScript { (Test-StringMatchRegEx -Item $_.Name -Matcher $RemoveGeneralInclude) -and !(Test-StringMatchRegEx -Item $_.Name -Matcher $RemoveGeneralExclude) } |
 		Sort-Object -Property 'Name'
 }
 If ($DockerProgramIsExist -and $RemoveDockerImageInclude.Count -gt 0 -and $OperationAsync) {
-	$DockerImageList += Receive-Job -Name "$SessionId/Docker/List" -Wait -AutoRemoveJob -ErrorAction 'Continue'
+	$DockerImageListRaw += Receive-Job -Name "$SessionId/Docker/List" -Wait -AutoRemoveJob -ErrorAction 'Continue'
 }
-$DockerImageRemoveQueue += $DockerImageList |
+$DockerImageRemoveQueue += $DockerImageListRaw |
+	Join-String -Separator ',' -OutputPrefix '[' -OutputSuffix ']' |
+	ConvertFrom-Json -Depth 100 |
+	ForEach-Object -Process { "$($_.Repository)$(($_.Tag.Length -gt 0) ? ":$($_.Tag)" : '')" } |
 	Where-Object -FilterScript { (Test-StringMatchRegEx -Item $_ -Matcher $RemoveDockerImageInclude) -and !(Test-StringMatchRegEx -Item $_ -Matcher $RemoveDockerImageExclude) } |
 	Sort-Object
 If ($GeneralRemoveQueue.Count -gt 0) {
@@ -257,11 +308,8 @@ If ($DockerImageRemoveQueue.Count -gt 0) {
 $Script:ErrorActionPreference = 'Continue'
 If ($DockerProgramIsExist) {
 	If ($OperationAsync) {
-		Write-Host -Object '[ASYNC] Remove Docker image and cache.'
-		$Null = Start-Job -Name "$SessionId/Docker/Optimize" -ScriptBlock {
-			ForEach ($_DI In $Using:DockerImageRemoveQueue) {
-				docker image rm $_DI *>&1
-			}
+		Write-Host -Object '[ASYNC] Remove Docker image.'
+		$Null = Start-Job -Name "$SessionId/Docker/Remove" -ScriptBlock $DockerCommandRemoveImage -ArgumentList (, $DockerImageRemoveQueue) {
 			If ($Using:RemoveDockerCache) {
 				docker image prune --force *>&1
 			}
@@ -271,13 +319,13 @@ If ($DockerProgramIsExist) {
 		If ($DockerImageRemoveQueue.Count -gt 0) {
 			ForEach ($_DI In $DockerImageRemoveQueue) {
 				Write-Host -Object "Remove Docker image ``$_DI``."
-				docker image rm $_DI *>&1 |
+				Invoke-Command -ScriptBlock $DockerCommandRemoveImage -ArgumentList (, $_DI) |
 					Write-GitHubActionsDebug
 			}
 		}
 		If ($RemoveDockerCache) {
 			Write-Host -Object 'Remove Docker cache.'
-			docker image prune --force *>&1 |
+			Invoke-Command -ScriptBlock $DockerCommandPruneImage |
 				Write-GitHubActionsDebug
 		}
 	}
@@ -319,14 +367,14 @@ Function Invoke-GeneralOptimizeOperation {
 			Where-Object -FilterScript {
 				$_.APT.Count -eq 0 -and $_.Chocolatey.Count -eq 0 -and $_.Homebrew.Count -eq 0 -and $_.NPM.Count -eq 0 -and $_.Pipx.Count -eq 0 -and (
 					$_.Env.Count -gt 0 -or
-					$_.($ListPathOsPropertyName).Count -gt 0
+					$_.($OsPathPropertyName).Count -gt 0
 				)
 			}
 		[PSCustomObject[]]$QueueFSRest = $Queue |
 			Where-Object -FilterScript {
 				($_.APT.Count -gt 0 -or $_.Chocolatey.Count -gt 0 -or $_.Homebrew.Count -gt 0 -or $_.NPM.Count -gt 0 -or $_.Pipx.Count -gt 0) -and (
 					$_.Env.Count -gt 0 -or
-					$_.($ListPathOsPropertyName).Count -gt 0
+					$_.($OsPathPropertyName).Count -gt 0
 				)
 			}
 		If ($APTProgramIsExist -and $QueueAPT.Count -gt 0) {
@@ -380,7 +428,7 @@ Function Invoke-GeneralOptimizeOperation {
 								Remove-Item -Recurse -Force -Confirm:$False -ErrorAction 'Continue'
 						}
 					}
-					ForEach ($Path In ($Using:FSPlain).($Using:ListPathOsPropertyName)) {
+					ForEach ($Path In ($Using:FSPlain).($Using:OsPathPropertyName)) {
 						[String]$PathResolve = ($Path -imatch '\$Env:') ? (Invoke-Expression -Command "`"$Path`"") : $Path
 						If ($PathResolve.Length -gt 0 -and (Test-Path -LiteralPath $PathResolve)) {
 							Get-ChildItem -LiteralPath $PathResolve -Force -ErrorAction 'Continue' |
@@ -402,7 +450,7 @@ Function Invoke-GeneralOptimizeOperation {
 								Remove-Item -Recurse -Force -Confirm:$False -ErrorAction 'Continue'
 						}
 					}
-					ForEach ($Path In ($Using:FSRest).($Using:ListPathOsPropertyName)) {
+					ForEach ($Path In ($Using:FSRest).($Using:OsPathPropertyName)) {
 						[String]$PathResolve = ($Path -imatch '\$Env:') ? (Invoke-Expression -Command "`"$Path`"") : $Path
 						If ($PathResolve.Length -gt 0 -and (Test-Path -LiteralPath $PathResolve)) {
 							Get-ChildItem -LiteralPath $PathResolve -Force -ErrorAction 'Continue' |
@@ -451,7 +499,7 @@ Function Invoke-GeneralOptimizeOperation {
 						Write-GitHubActionsDebug
 				}
 			}
-			If ($Item.Env.Count -gt 0 -or $Item.($ListPathOsPropertyName).Count -gt 0) {
+			If ($Item.Env.Count -gt 0 -or $Item.($OsPathPropertyName).Count -gt 0) {
 				Write-Host -Object "Remove $($Item.Description) files."
 				ForEach ($EnvName In $Item.Env) {
 					[String]$EnvValue = Get-Content -LiteralPath "Env:\$EnvName" -ErrorAction 'SilentlyContinue'
@@ -460,7 +508,7 @@ Function Invoke-GeneralOptimizeOperation {
 							Remove-Item -Recurse -Force -Confirm:$False -ErrorAction 'Continue'
 					}
 				}
-				ForEach ($Path In $Item.($ListPathOsPropertyName)) {
+				ForEach ($Path In $Item.($OsPathPropertyName)) {
 					[String]$PathResolve = ($Path -imatch '\$Env:') ? (Invoke-Expression -Command "`"$Path`"") : $Path
 					If ($PathResolve.Length -gt 0 -and (Test-Path -LiteralPath $PathResolve)) {
 						Get-ChildItem -LiteralPath $PathResolve -Force -ErrorAction 'Continue' |
@@ -515,12 +563,48 @@ If ($OsIsLinux -and $RemoveLinuxSwap) {
 		Write-GitHubActionsDebug
 	Remove-Item -LiteralPath '/mnt/swapfile' -Recurse -Force -Confirm:$False -ErrorAction 'Continue'
 }
-$Script:ErrorActionPreference = 'Stop'
 If (Get-GitHubActionsDebugStatus) {
 	Enter-GitHubActionsLogGroup -Title 'Program Tree (After): '
 	Get-ProgramTree
 	Exit-GitHubActionsLogGroup
+	If ($APTProgramIsExist) {
+		Enter-GitHubActionsLogGroup -Title 'APT (After): '
+		Invoke-Command -ScriptBlock $APTCommandListPackage |
+			Write-Host
+		Exit-GitHubActionsLogGroup
+	}
+	If ($ChocolateyProgramIsExist) {
+		Enter-GitHubActionsLogGroup -Title 'Chocolatey (After): '
+		Invoke-Command -ScriptBlock $ChocolateyCommandListPackage |
+			Write-Host
+		Exit-GitHubActionsLogGroup
+	}
+	If ($DockerProgramIsExist) {
+		Enter-GitHubActionsLogGroup -Title 'Docker (After): '
+		Invoke-Command -ScriptBlock $DockerCommandListImage |
+			Write-Host
+		Exit-GitHubActionsLogGroup
+	}
+	If ($HomebrewProgramIsExist) {
+		Enter-GitHubActionsLogGroup -Title 'Homebrew (After): '
+		Invoke-Command -ScriptBlock $HomebrewCommandListPackage |
+			Write-Host
+		Exit-GitHubActionsLogGroup
+	}
+	If ($NPMProgramIsExist) {
+		Enter-GitHubActionsLogGroup -Title 'NPM (After): '
+		Invoke-Command -ScriptBlock $NPMCommandListPackage |
+			Write-Host
+		Exit-GitHubActionsLogGroup
+	}
+	If ($PipxProgramIsExist) {
+		Enter-GitHubActionsLogGroup -Title 'Pipx (After): '
+		Invoke-Command -ScriptBlock $PipxCommandListPackage |
+			Write-Host
+		Exit-GitHubActionsLogGroup
+	}
 }
+$Script:ErrorActionPreference = 'Stop'
 [String]$DiskSpaceAfter = Get-DiskSpace
 Write-Host -Object @"
 ===== DISK SPACE BEFORE =====
